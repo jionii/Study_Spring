bb9878649d19bf7d6793e97cb2430a672119ceae
jionii <athena020219@naver.com>
2025-06-09 15:31:34 +0900

    0609-스프링 오후 강의

--- ex01/src/main/java/org/scoula/beans/Person.java
+++ ex01/src/main/java/org/scoula/beans/Person.java
@@ -1,9 +1,16 @@
 package org.scoula.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Person {
 
     private String name;
+
     private Parrot parrot;
+
+    // 필드 주입
+//    @Autowired
+//    private Parrot parrot;
 
     public Parrot getParrot() {
         return parrot;
@@ -13,6 +20,7 @@ public class Person {
         return name;
     }
 
+    // Set을 사용한 의존성 주입
     public void setParrot(Parrot parrot) {
         this.parrot = parrot;
     }
