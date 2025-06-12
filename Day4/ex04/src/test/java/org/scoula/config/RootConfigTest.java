b9da151332510ac343fde2ace2dd1ae0ec52d1e9
jionii <athena020219@naver.com>
2025-06-12 13:49:42 +0900

    0612-mybatis 설정

---
+++ Day4/ex04/src/test/java/org/scoula/config/RootConfigTest.java
@@ -0,0 +1,58 @@
+package org.scoula.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes= {RootConfig.class})
+@Log4j2
+class RootConfigTest {
+
+    // 주입 3가지 --> 필드 위에 주입!
+    @Autowired
+    private DataSource dataSource; // hikariDBCP 들어감
+
+    @Test
+    @DisplayName("DataSource 연결 정보 확인")
+    public void dataSource() throws SQLException {
+        try(Connection con = dataSource.getConnection()){
+            log.info("DataSource 준비 완료");
+            log.info(con);
+        }
+
+//        Connection con = dataSource.getConnection();
+//        log.info("dbcp로 부터 연결하나 가지고 왔음");
+//        log.info(con);
+//        con.close(); // ===> 안 쓴다 라고 dbcp 객체로 알려줌
+    }
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void testSqlSessionFactory() {
+        try (
+                SqlSession session = sqlSessionFactory.openSession();
+                Connection con = session.getConnection();
+        ) {
+            log.info(session);
+            log.info(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
---
+++ Day4/ex04/src/test/java/org/scoula/mapper/TimeMapperTest.java
@@ -0,0 +1,30 @@
+package org.scoula.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.scoula.config.RootConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { RootConfig.class })
+class TimeMapperTest {
+
+    @Autowired
+    private TimeMapper timeMapper;
+
+    @Test
+    @DisplayName("TimeMapper의 getTime2()")
+    public void getTime2() {
+
+        log.info("getTime2");
+        log.info(timeMapper.getTime2());
+
+    }
+}
